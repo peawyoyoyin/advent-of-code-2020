1a55a00eb2fedc9a945c4294a24867a0e1ca72b5
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2020-12-08 23:36:15 +0700

    add (hopefully readable) explanation of the optimal solution

--- problems/day8/prob2-opt.ts
+++ problems/day8/prob2-opt.ts
@@ -4,6 +4,24 @@ import { executeInstructions, instructions as _instructions } from "./common";
 function solve(instructions: typeof _instructions) {
     let indexToSwap = -1;
 
+    /*
+     explanation: DP approach to find the index we have to swap between jmp and nop
+     recursion function: is F(N: number, W: number, V: set) where
+        F(n, *, *) = true if n equals the length of all instructions (means the machine stops at index n)
+        F(j, *, V) = false if j in V (means we are running index j for the second time)
+
+        otherwise let i, p be the instruction and parameter of the nth instruction
+        F(n, w, v) equals the following:
+            - F(n+1, w, v U n) if i == 'acc'
+                - explanation: if 'acc' we just go to next index
+            - F(n+1, w, v U n) || (F(n+p, w, v U n) && w == -1) if i == 'nop'
+                - explanation: if 'nop' we try next index, and if w == -1 (means not swapped yet) we also try jumping ('jmp')
+            - F(n+p, w, v U n) || (F(n+1, w, v U n) && w == -1) if i == 'acc'
+                - explanation: if 'jmp' we try jumping, and if w == -1 (means not swapped yet) we also try next index ('nop')
+        
+        then we add memoization with mapping from (s: number, t: boolean) -> w: boolean
+            where s = index, t = (w == -1), w = return value of F 
+    */
     const memo = new Map<string, boolean>();
     function dyn(index: number, swapped: number, visited: Set<number>): boolean {
         const memoKey = `${index}:${swapped === -1}`; 
